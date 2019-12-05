64a4acd5c63a6cf7240f16610f8f5371d081c701
bj0rnps <55537500+bj0rnps@users.noreply.github.com>
2019-12-05 16:34:16 +0100

    Update p360addin.ps1

--- p360addin.ps1
+++ p360addin.ps1
@@ -1,6 +1,8 @@
-   [string]$addin='SI.Outlook.Sidebar.Container'
-   [string]$regpath='HKCU:\Software\Microsoft\Office\16.0\Outlook\Resiliency\DoNotDisableAddinList'
-   [int]$value=1
-   
-   New-ItemProperty -Path $regpath -Name $addin -PropertyType dword -Value $value -WhatIf
-   
+
+
+[string]$addin='SI.Outlook.Sidebar.Container'
+[string]$keypath='HKCU:\Software\Microsoft\Office\16.0\Outlook\Resiliency'
+[string]$regpath='HKCU:\Software\Microsoft\Office\16.0\Outlook\Resiliency\DoNotDisableAddinList'
+[int]$value=1
+New-Item -Name 'DoNotDisableAddinList' -Path $keypath
+New-ItemProperty -Path $regpath -Name $addin -PropertyType dword -Value $value
