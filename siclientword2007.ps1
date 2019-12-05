abce79c84b069adb313f68c9957b6c7428f820bf
bj0rnps <55537500+bj0rnps@users.noreply.github.com>
2019-12-05 16:29:13 +0100

    Create siclientword2007.ps1

---
+++ siclientword2007.ps1
@@ -0,0 +1,41 @@
+
+
+###word
+
+#common
+[string]$addin='SI.Client.Word2007'
+
+#Resiliency
+
+[string]$Resiliency_key= 'HKCU:\Software\Microsoft\Office\16.0\Word\Resiliency\'
+[string]$DoNotDisableAddinList_key='HKCU:\Software\Microsoft\Office\16.0\Word\Resiliency\DoNotDisableAddinList'#test this path
+[int]$Resiliency_value=1
+
+#Load behavior
+[string]$loadbehavior_key='HKCU:\SOFTWARE\Microsoft\Office\Word\addins\'+$addin #test this path
+[string]$addinlist_key='HKCU:\SOFTWARE\Microsoft\Office\Word\addins'
+[int]$loadbehavior_value=3 
+
+
+
+
+
+if (!(Test-Path $DoNotDisableAddinList_key)) {
+
+New-Item -Path $DoNotDisableAddinList_key -Name 'DoNotDisableAddinList'
+}
+
+if (!(Test-Path $loadbehavior_key)){
+   New-Item -Path $loadbehavior_key                                         
+      
+ }
+ if (!(Test-Path ($loadbehavior_key) )){
+   New-Item -Path ($loadbehavior_key )
+
+  }
+
+New-ItemProperty -Path $DoNotDisableAddinList_key -Name $addin -Value $Resiliency_value
+
+New-ItemProperty -Path ($loadbehavior_key)  -Name 'LoadBehavior' -Value $loadbehavior_value
+
+
