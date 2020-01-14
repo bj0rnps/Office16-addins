57d6de1f0396e2ae3aa0c0e490cfaf02beee085b
bj0rnps <55537500+bj0rnps@users.noreply.github.com>
2020-01-14 09:12:32 +0100

    Update outlook-skypeaddin.ps1

--- outlook-skypeaddin.ps1
+++ outlook-skypeaddin.ps1
@@ -1,6 +1,6 @@
-   ##
+
    [string]$addin='UcAddin.LyncAddin.1'
    [string]$regpath='HKCU:\Software\Microsoft\Office\16.0\Outlook\Resiliency\DoNotDisableAddinList'
    [int]$value=1
    
-   New-ItemProperty -Path $regpath -Name $addin -PropertyType dword -Value $value -WhatIf
+   New-ItemProperty -Path $regpath -Name $addin -PropertyType dword -Value $value 
